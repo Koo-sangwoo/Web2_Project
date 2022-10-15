9c0ae2866f6b4e1c60fabce9d2c75e079537c97b
한수민 <pppplb95@gmail.com>
2022-10-15 16:52:48 +0900

    product

--- Web_ShoppingMall/src/main/java/com/shopping/web2/vo/ProductVO.java
+++ Web_ShoppingMall/src/main/java/com/shopping/web2/vo/ProductVO.java
@@ -1,17 +1,24 @@
 package com.shopping.web2.vo;
 
 public class ProductVO {
-//	create table if not exists product(
-//			 productId varchar(100) not null    comment '상품아이디(pk)',
-//			 pname   varchar(100)   comment '상품명',
-//			 price   varchar(100)   comment '상품 가격',
-//			 pdesc   varchar(100)   comment '상품 설명',
-//			 category varchar(100)   comment '상품분류',
-//			 filenamemember   varchar(100) comment '이미지파일' ,
-//			 SunitStock varchar(100) comment 'S사이즈 재고',
-//			 MunitStock varchar(100) comment 'M사이즈 재고',
-//			 LunitStock varchar(100) comment 'L사이즈 재고',
-//			 XLunitStock varchar(100) comment 'XL 사이즈 재고',
-//			 primary key (productId)
-//			)default CHARSET = utf8;
+
+	private String productId;
+	private String pname;
+	private String price;  
+	private String pdesc;  
+	private String category; 
+	private String filenamemember;
+	private String SunitStock;
+	private String MunitStock;
+	private String LunitStock;
+	private String XLunitStock;
+	@Override
+	public String toString() {
+		return "ProductVO [productId=" + productId + ", pname=" + pname + ", price=" + price + ", pdesc=" + pdesc
+				+ ", category=" + category + ", filenamemember=" + filenamemember + ", SunitStock=" + SunitStock
+				+ ", MunitStock=" + MunitStock + ", LunitStock=" + LunitStock + ", XLunitStock=" + XLunitStock + "]";
+	}
+	
+	
+
 }
