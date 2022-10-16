cecbef08d6f395c8756f9a30ffa21aa811e20177
C-12 <C-12@DESKTOP-ITKTR16>
2022-10-16 14:51:57 +0900

    ProductController 수정

--- Web_ShoppingMall/src/main/java/com/shopping/web2/controller/ProductController.java
+++ Web_ShoppingMall/src/main/java/com/shopping/web2/controller/ProductController.java
@@ -1,5 +1,19 @@
 package com.shopping.web2.controller;
 
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class ProductController {
+	@RequestMapping(value="/product", method = RequestMethod.GET)
+	public ModelAndView product(Locale locale, Model model) {
+		ModelAndView mav = new ModelAndView("product/productList");
+		return mav;
+	}
 
 }
