7984233fad56a6a4d65a2b616c254691e527ab7b
C-12 <C-12@DESKTOP-ITKTR16>
2022-10-16 17:43:01 +0900

    ProductDao 수정

--- Web_ShoppingMall/src/main/java/com/shopping/web2/dao/ProductDao.java
+++ Web_ShoppingMall/src/main/java/com/shopping/web2/dao/ProductDao.java
@@ -1,5 +1,21 @@
 package com.shopping.web2.dao;
 
-public class ProductDao {
+import java.util.List;
+import java.util.Map;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.shopping.web2.vo.MemberVO;
+
+@Repository
+public class ProductDao {
+	@Autowired
+	private SqlSessionTemplate sqltemplate;
+	
+	public List<MemberVO> list(Map<String, Object> map){
+		return sqltemplate.selectList("product.list",map);
+	}
+	
 }
